bffb4e4cd6e99f97b320b3ff5f85ddb51798d650
Victor Souza <taffavitoa710@gmail.com>
2022-02-27 00:34:09 -0300

    fixing app.js :smile:

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
 
 app.listen(PORT, () => {
-	console.log( process.env.hostname + ":" + PORT)
+	console.log( "PORT = " + PORT)
 });
 
