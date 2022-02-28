e508a981f7b4176b879149fc730129c318a18143
victor0x16 <100092662+victor0x16@users.noreply.github.com>
2022-02-28 01:24:36 -0300

    Display fake access token
    😄

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send(process.env.ACCESS_TOKEN);
 })
 
 app.listen(PORT, () => {
