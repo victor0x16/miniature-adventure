228bb1a99936e70ea7a8d7401d730ba9908e69f2
victor0x16 <100092662+victor0x16@users.noreply.github.com>
2022-02-28 01:34:53 -0300

    adding URL
    😃

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+// curl -X POST https://api.twitter.com/2/tweets -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-type: application/json" -d '{"text": "Hello World!"}'
 
 const PORT = process.env.PORT || 5000;
 
