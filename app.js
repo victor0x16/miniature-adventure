a16abd1dc78b3a900676a4cccaac7bf914d46d2d
Victor Souza <taffavitoa710@gmail.com>
2022-03-09 00:43:05 -0300

    update api and add assets

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
 app.get('/', function (req, res) {
-  res.send("Hello, World!");
-})
+  res.send("404 - page has changed!");
+});
+
+
+// api
+app.get("/api/interactions", function (req, res) {
+   res.send({ test: "you're in a test!" });
+});
+
+app.get("/terms-of-service", function (req, res) {
+  res.sendFile("/assets/index.html");
+});
+
+
+app.get("/privacy-policy", function (req, res) {
+  res.sendFile("/assets/index.html");
+});
 
 app.listen(PORT, () => {
 	console.log( "PORT = " + PORT)
